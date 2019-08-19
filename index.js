19e304ece8502d79cf7f85bdf64a1aedff694732
CarloC24 <carlo.clamucha@gmail.com>
2019-08-18 23:00:50 -0500

    removed router

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const server = express();
-const personRouter = require("./personrouter");
+// const personRouter = require("./personrouter");
 
 const port = process.env.PORT || 5000;
 // console.log(someundefined);
 server.use(express.json());
-server.use("/person", personRouter);
+// server.use("/person", personRouter);
 server.get("/", (req, res) => {
   res.json({ message: "Hello" });
 });
