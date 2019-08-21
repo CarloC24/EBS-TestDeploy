5111c9b4f984517948ec409bbc34674fa1977ad2
CarloC24 <carlo.clamucha@gmail.com>
2019-08-20 21:28:50 -0500

    adding stuff

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const server = express();
+const cors = require("cors");
 const personRouter = require("./personrouter");
 
 const port = process.env.PORT || 5000;
 // console.log(someundefined);
 server.use(express.json());
+server.use(cors());
 server.use("/person", personRouter);
 server.get("/", (req, res) => {
   res.json({ message: "Hello" });
+});
+server.post("/formdata", (req, res) => {
+  console.log(req.body, "body");
+  res.json({ message: "recieved formdata" });
 });
 
 server.listen(port, () => {
